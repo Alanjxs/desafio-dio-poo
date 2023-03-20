e9d1f2bfcd74da33fb23f47dd43bf7a2d6ae4e4d
alanjxs@gmail.com <alanjxs@gmail.com>
2023-03-20 16:03:38 -0300

    Finalização do Desafio dio poo

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -2,7 +2,9 @@ package br.com.dio.desafio;
 
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -23,12 +25,43 @@ public class Main {
 		mentoria1.setDescricao("Descrição Mentoria JAVA");
 		mentoria1.setData(LocalDate.now());
 		
-		
-		
-		System.out.println(curso1);
+		/*System.out.println(curso1);
 		System.out.println(curso2);
-		System.out.println(mentoria1);
+		System.out.println(mentoria1);*/
 		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp JAVA Developer");
+		bootcamp.setDescricao("Descrição Bootcamp JAVA Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria1);
+		
+		Dev devAlan = new Dev();
+		devAlan.setNome("Alanjxs");
+		devAlan.inscreverBootcamp(bootcamp);
+		System.out.println("Alanjxs - Conteúdos inscritos: " + devAlan.getConteudosInscritos());
+		System.out.println("Alanjxs - Conteúdos concluídos: " + devAlan.getConteudosConcluidos());
+		System.out.println("XP Inicial: " + devAlan.calcularTotalXp());	
+		devAlan.progredir();
+		devAlan.progredir();
+		System.out.println("-------------");	
+		System.out.println("Alanjxs - Conteúdos concluídos: " + devAlan.getConteudosConcluidos());
+		System.out.println("Alanjxs - Conteúdos inscritos: " + devAlan.getConteudosInscritos());
+		System.out.println("XP Final: " + devAlan.calcularTotalXp());
+		
+		System.out.println("-------------");
+		Dev devCamila = new Dev();
+		devCamila.setNome("Camila");
+		devCamila.inscreverBootcamp(bootcamp);
+		System.out.println("Camila - Conteúdos inscritos: " + devCamila.getConteudosInscritos());
+		System.out.println("Camila - Conteúdos concluídos: " + devCamila.getConteudosConcluidos());
+		System.out.println("XP Inicial: " + devCamila.calcularTotalXp());
+		devCamila.progredir();
+		System.out.println("-------------");
+		System.out.println("Camila - Conteúdos inscritos: " + devCamila.getConteudosInscritos());
+		System.out.println("Camila - Conteúdos concluídos: " + devCamila.getConteudosConcluidos());
+		System.out.println("XP Atual: " + devCamila.calcularTotalXp());
+	
 
 	}
 
